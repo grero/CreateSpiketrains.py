10d777da146d8e36b4920808c9ea6501f5fa86b2
Roger Herikstad <roger.herikstad@gmail.com>
2019-12-02 16:04:58 +0800

    Some fixes for python3

--- CreateSpiketrains/create_spiketrains.py
+++ CreateSpiketrains/create_spiketrains.py
@@ -119,8 +119,8 @@ class ViewWidget(QMainWindow):
             self.merged_lines.remove(label)
             self.merged_colors.remove(self.merged_colors[index])
 
-        print "Save individual spiketrains for waveforms: ", self.picked_lines
-        print "Save merged spiketrains for waveforms: ", self.merged_lines
+        print("Save individual spiketrains for waveforms: ", self.picked_lines)
+        print("Save merged spiketrains for waveforms: ", self.merged_lines)
         artist.figure.canvas.draw()
 
     def plot_waveforms(self, waveforms):
@@ -128,13 +128,13 @@ class ViewWidget(QMainWindow):
         sd = 4
         noise = sd*math.sqrt(1/self.cinv) # calculates standard deviation
         if (self.ishdf5 == True):
-            print "this is hdf5"
-            for i in xrange(waveforms.shape[2]):
+            print("this is hdf5")
+            for i in range(waveforms.shape[2]):
                 ax.axhline(y=noise, color='k')
                 ax.axhline(y=-noise, color='k')
                 p = ax.plot(waveforms[:, 0, i], label="Waveform %d" % (i, ), picker=5)
         else:
-            print "this is not hdf5"
+            print("this is not hdf5")
             for i in xrange(waveforms.shape[0]):
                 ax.axhline(y=noise, color='k')
                 ax.axhline(y=-noise, color='k')
@@ -145,7 +145,7 @@ class ViewWidget(QMainWindow):
         tot_timestamps = []
         num_timestamps = []
         merge_timestamps = [] # for all merged waveforms
-        print "Saving spiketrains"
+        print("Saving spiketrains")
         with SaveFile(self.sortfile) as qq:
             if ("samplingRate" not in qq.keys()) and ("samplingrate" not in qq.keys()):
                 if self.sampling_rate == -1.0:
